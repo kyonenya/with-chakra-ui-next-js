091cb7807fb962f2e067074dbffd2e9b0bcaedfb
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-04-30 21:47:04 +0900

    feat: Submitボタン

--- src/components/NavBarWithDropdown.tsx
+++ src/components/NavBarWithDropdown.tsx
@@ -16,7 +16,13 @@ import {
   useColorModeValue,
   Stack,
 } from '@chakra-ui/react';
-import { ArrowBackIcon, HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import {
+  ArrowBackIcon,
+  CheckIcon,
+  CloseIcon,
+  TriangleUpIcon,
+  TriangleDownIcon,
+} from '@chakra-ui/icons';
 
 const Links = ['Dashboard', 'Team'];
 
@@ -44,33 +50,19 @@ export default function Simple() {
           <IconButton
             size={'md'}
             icon={<ArrowBackIcon />}
-            aria-label={'Open Menu'}
+            aria-label={'Back to Top'}
             onClick={isOpen ? onClose : onOpen}
           />
+
           <HStack spacing={8} alignItems={'center'}>
-            <Box>Logo</Box>
-            <HStack
-              as={'nav'}
-              spacing={4}
-            >
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}              
-            </HStack>
-          </HStack>          
-          <Flex alignItems={'center'}>
             <Menu>
               <MenuButton
                 as={Button}
                 rounded={'full'}
                 variant={'link'}
-                cursor={'pointer'}>
-                <Avatar
-                  size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
-                />
+                cursor={'pointer'}
+              >
+                <TriangleDownIcon />
               </MenuButton>
               <MenuList>
                 <MenuItem>Link 1</MenuItem>
@@ -79,18 +71,16 @@ export default function Simple() {
                 <MenuItem>Link 3</MenuItem>
               </MenuList>
             </Menu>
-          </Flex>
-        </Flex>
+          </HStack>
 
-        {/* {isOpen ? (
-          <Box pb={4}>
-            <Stack as={'nav'} spacing={4}>
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}
-            </Stack>
-          </Box>
-        ) : null} */}
+          <Flex alignItems={'center'}>
+            <IconButton
+              size={'md'}
+              icon={<CheckIcon />}
+              aria-label={'Submit'}
+            />
+          </Flex>          
+        </Flex>
       </Box>
 
       <Box p={4}>Main Content Here</Box>
