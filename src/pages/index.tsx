bda8a8643c65ced1d6f54140b338631c47078e12
kyonenya <kyon.musica@gmail.com>
2021-04-30 04:20:22 +0000

    feat: ヘッダーメニューテンプレート

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import { Box, useColorMode } from '@chakra-ui/react'
 import GrayBox from '../components/GrayBox'
 import SomeForm from '../components/SomeForm'
 import DarkModeSwitch from '../components/DarkModeSwitch2'
+import NavBarWithDropdown from '../components/NavBarWithDropdown'
 
 const Home = () => {
   const { colorMode } = useColorMode()
   return (
     <>
       <DarkModeSwitch />
+      <NavBarWithDropdown />
       <SomeForm />
     </>
   )
