77ba716732339295ea7d69c53cea92c1059dd37f
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-05-01 21:19:21 +0900

    feat: トップページのヘッダーと検索フォーム

---
+++ src/components/NavBarWithSearchForm.tsx
@@ -0,0 +1,95 @@
+import { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  InputGroup,
+  Input,
+  InputRightElement,
+  InputLeftElement,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react';
+import {
+  ArrowBackIcon,
+  CheckIcon,
+  CloseIcon,
+  SearchIcon,
+  TriangleUpIcon,
+  TriangleDownIcon,
+} from '@chakra-ui/icons';
+
+const Links = ['Dashboard', 'Team'];
+
+const NavLink = ({ children }: { children: ReactNode }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'#'}>
+    {children}
+  </Link>
+);
+
+export default function Simple() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={<ArrowBackIcon />}
+            aria-label={'Back to Top'}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+
+    <InputGroup size="md">
+      <InputLeftElement>
+        <SearchIcon
+          color={useColorModeValue('gray.500', 'gray.500')}
+        >
+        </SearchIcon>
+      </InputLeftElement>
+      <Input
+        type="text"
+        placeholder="Enter Search Word"
+        aria-label="Serach Form"
+        borderColor={useColorModeValue('gray.300', 'gray.700')}
+      />
+    </InputGroup>
+    
+          </HStack>
+
+          <Flex alignItems={'center'}>
+            <IconButton
+              size={'md'}
+              icon={<CheckIcon />}
+              aria-label={'Submit'}
+            />
+          </Flex>          
+        </Flex>
+      </Box>
+    </>
+  );
+}
+
+/**
+ * @url https://chakra-templates.dev/navigation/navbar
+ */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import GrayBox from '../components/GrayBox'
 import SomeForm from '../components/SomeForm'
 import DarkModeSwitch from '../components/DarkModeSwitch2'
 import NavBarWithDropdown from '../components/NavBarWithDropdown'
+import NavBarWithSearchForm from '../components/NavBarWithSearchForm'
 import BlogArticleList from '../components/BlogArticleList'
 import PostList from '../components/PostList'
 import TagCard from '../components/TagCard'
@@ -12,7 +13,7 @@ const Home = () => {
   return (
     <>
       <DarkModeSwitch />
-      <NavBarWithDropdown />
+      <NavBarWithSearchForm />
       <PostList />
     </>
   )
